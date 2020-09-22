a5dccbcd3fa4da77b1a747583240431762572231
aldo66lim <kristianrolandolim66@gmail.com>
2020-09-22 02:15:36 -0700

    Ini Biodata

---
+++ Biodata.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+int main()
+{
+	system("color A");
+	
+	printf("\tBiodata Diri\n\n");
+	printf("Nama          : Kristian Rolando Limantara\n");
+	printf("NPM           : 19081010085\n");
+	printf("Jurusan       : Informatika\n");
+	printf("Fakultas      : Fakultas Ilmu Komputer\n");
+	printf("Tanggal Lahir : 06 Juni 2001");
+	getch();
+}
